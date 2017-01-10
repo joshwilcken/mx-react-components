1133571f1b32a12eebc9d15fe2b5325caf01dc28
lukeschunk <luke.schunk@mx.com>
2017-01-10 11:10:30 -0700

    messageBox expanding

--- src/constants/Style.js
+++ src/constants/Style.js
@@ -132,7 +132,6 @@ module.exports = {
     return (usePound ? '#' : '') + (g | (b << 8) | (r << 16)).toString(16);
   },
 
-
   adjustHexOpacity (color, opacity) {
     const r = parseInt(color.slice(1, 3), 16);
     const g = parseInt(color.slice(3, 5), 16);
